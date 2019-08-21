58aeea9db283a3f034a057e7929733b8c161c47a
hyunwook <c004245@naver.com>
2019-08-21 11:59:30 +0900

    adapter 연결 , ViewModel update UI 처리

--- app/src/main/java/com/example/mvvmtesting/MainActivity.kt
+++ app/src/main/java/com/example/mvvmtesting/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mvvmtesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmtesting.model.Contact
+import com.example.mvvmtesting.view.ContactAdapter
 import com.example.mvvmtesting.viewmodel.ContactViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +19,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val adapter = ContactAdapter({ contact ->
+
+        }, { contact ->
+            deleteDialog(contact)
+        })
+
+        val lm = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = lm
+        recyclerView.setHasFixedSize(true)
+
+
         contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java)
         contactViewModel.getAll().observe(this, Observer<List<Contact>> { contacts ->
             //Update UI
+            adapter.setContacts(contacts!!)
         })
+    }
+
+    private fun deleteDialog(contact: Contact) {
+        val builder = AlertDialog.Builder(this)
+            .setNegativeButton("No") { _, _ -> }
+            .setPositiveButton("YES") { _, _ ->
+                contactViewModel.delete(contact)
+            }
+        builder.show()
     }
 }
