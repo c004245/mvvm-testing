cf7203a9600b4f19841e1aeabbd40514dc52c068
hyunwook <c004245@naver.com>
2019-08-21 11:32:32 +0900

    Viewmodel observe List<Contact>

--- app/src/main/java/com/example/mvvmtesting/MainActivity.kt
+++ app/src/main/java/com/example/mvvmtesting/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.mvvmtesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.mvvmtesting.model.Contact
+import com.example.mvvmtesting.viewmodel.ContactViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var contactViewModel: ContactViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contactViewModel = ViewModelProviders.of(this).get(ContactViewModel::class.java)
+        contactViewModel.getAll().observe(this, Observer<List<Contact>> { contacts ->
+            //Update UI
+        })
     }
 }
