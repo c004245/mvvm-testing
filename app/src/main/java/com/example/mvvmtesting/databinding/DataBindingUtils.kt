4e04fc08a16b08298642c044579d06d05c3bf0e9
hyunwook <c004245@naver.com>
2019-08-22 18:36:22 +0900

    DataBinding BindingAdapter RecyclerView 환경 구성

---
+++ app/src/main/java/com/example/mvvmtesting/databinding/DataBindingUtils.kt
@@ -0,0 +1,25 @@
+package com.example.mvvmtesting.databinding
+
+import android.content.ClipData
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mvvmtesting.model.Contact
+import com.example.mvvmtesting.view.ContactAdapter
+
+@BindingAdapter("setAdapter")
+fun bindRecyclerViewAdapter(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) {
+    recyclerView.setHasFixedSize(true)
+    recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+    recyclerView.adapter = adapter
+}
+
+@BindingAdapter("bind_items")
+fun setBindItem(view: RecyclerView, items: List<Contact>?) {
+    items?.let {
+        val adapter = view.adapter as ContactAdapter
+        adapter.setContacts(items)
+        adapter.notifyDataSetChanged()
+    }
+}
