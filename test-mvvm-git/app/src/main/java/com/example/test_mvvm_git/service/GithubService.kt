73ccea91731a136bdd1b503fd711934c1ddf6d9b
hyunwook <c004245@naver.com>
2019-09-03 11:26:46 +0900

    Retrofit Gradle 추가, Github Restful api Service추가

---
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/service/GithubService.kt
@@ -0,0 +1,17 @@
+package com.example.test_mvvm_git.service
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubService {
+
+    val HTTPS_API_GITHUB_URL
+        get() = "https://api.github.com/"
+
+    @GET("users/{user}/repos")
+    fun getProjectList(@Path("user") user: String): Call<List<Project>>
+
+    @GET("/repos/{user}/{reponame}")
+    fun getProjectDetails(@Path("user") user: String, @Path("reponame") projectName: String): Call<Project>
+}
