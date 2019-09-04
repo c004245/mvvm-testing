547aaa3ee7489c9b69b52e2124858b99014c4fa5
hyunwook <c004245@naver.com>
2019-09-04 12:44:06 +0900

    LiveData Need ViewModel, 깃허브 리스트 호출

---
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/service/ProjectRepository.kt
@@ -0,0 +1,26 @@
+package com.example.test_mvvm_git.service
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ProjectRepository {
+    private lateinit var githubService: GithubService
+
+    fun getProjectList(userId: String): LiveData<List<Project>> {
+        val data = MutableLiveData<List<Project>>()
+
+        githubService.getProjectList(userId).enqueue(object :Callback<List<Project>>() {
+            override fun onResponse(call: Call<List<Project>>?, response: Response<List<Project>>?) {
+                data.value = response!!.body()
+            }
+
+            override fun onFailure(call: Call<List<Project>>?, t: Throwable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+        return data
+    }
+}
---
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/viewmodel/ProjectListViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.test_mvvm_git.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.test_mvvm_git.service.ProjectRepository
+
+class ProjectListViewModel(private val app: Application): AndroidViewModel(app) {
+
+    private val projectListObservable: LiveData<List<Project>>
+
+    init {
+        projectListObservable = ProjectRepository.getInstance().getProjectList("Google")
+    }
+
+    fun getProjectListObservable(): LiveData<List<Project>> {
+        return projectListObservable
+    }
+    
+}
