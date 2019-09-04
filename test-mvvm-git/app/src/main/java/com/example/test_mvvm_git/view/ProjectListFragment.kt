c6780cabac7b8d65eadb46dd72f38142f3b34bd8
hyunwook <c004245@naver.com>
2019-09-04 14:42:02 +0900

    Lifecycle Gradle 추가, ListFragment에 ViewModel 연결

---
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/view/ProjectListFragment.kt
@@ -0,0 +1,31 @@
+package com.example.test_mvvm_git.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.test_mvvm_git.viewmodel.ProjectListViewModel
+
+class ProjectListFragment: Fragment {
+    lateinit var projectAdapter: ProjectAdapter
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val viewModel = ViewModelProviders.of(this).get(ProjectListViewModel::class.java)
+
+        observeViewModel(viewModel)
+    }
+
+    fun observeViewModel(viewModel: ProjectListViewModel) {
+        viewModel.getProjectListObservable().observe(this, object : Observer<List<Project>> {
+            override fun onChanged(projects: List<Project>?) {
+                if (projects != null) {
+                    projectAdapter.setProjectList(projects)
+                }
+            }
+        })
+    }
+
+}
