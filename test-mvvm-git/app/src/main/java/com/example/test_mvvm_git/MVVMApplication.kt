3f373be2088c9165bd455f33a962c868196f5648
hyunwook <c004245@naver.com>
2019-09-05 11:34:57 +0900

    Application

--- test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/MVVMApplication.kt
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/MVVMApplication.kt
@@ -1,7 +1,23 @@
 package com.example.test_mvvm_git
 
+import android.app.Activity
 import android.app.Application
+import com.example.test_mvvm_git.di.AppInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
 
-class MVVMApplication: Application() {
+class MVVMApplication: Application(), HasActivityInjector {
 
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun onCreate() {
+        super.onCreate()
+        AppInjector.init(this)
+    }
+
+    override fun activityInjector(): DispatchingAndroidInjector<Activity> {
+        return dispatchingAndroidInjector
+    }
 }
