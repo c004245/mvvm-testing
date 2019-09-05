1c30ba8131353815bce9fffffd8237301adfcc30
hyunwook <c004245@naver.com>
2019-09-05 11:24:37 +0900

    Add Activity Module, Component add

---
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/MVVMApplication.kt
@@ -0,0 +1,7 @@
+package com.example.test_mvvm_git
+
+import android.app.Application
+
+class MVVMApplication: Application() {
+
+}
--- test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/di/AppComponent.kt
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/di/AppComponent.kt
@@ -1,15 +1,28 @@
 package com.example.test_mvvm_git.di
 
+import android.app.Application
+import com.example.test_mvvm_git.MVVMApplication
+import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [
-    AndroidInjectionModule::class,
-    AppModule::class,
-    MainActivityModule::class])
-interface AppComponent  {
-    
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        AppModule::class,
+        MainActivityModule::class]
+)
+interface AppComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun appliation(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(mvvmApplication: MVVMApplication)
 }
 
