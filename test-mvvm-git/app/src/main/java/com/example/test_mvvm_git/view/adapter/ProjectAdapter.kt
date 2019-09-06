e6974f95e7ef71eef0c52fb82a8eda8d3d8dbb7f
hyunwook <c004245@naver.com>
2019-09-06 19:23:30 +0900

    convert java to koltin on Adapter class

--- test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/view/adapter/ProjectAdapter.kt
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/view/adapter/ProjectAdapter.kt
@@ -3,33 +3,66 @@ package com.example.test_mvvm_git.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test_mvvm_git.R
 import com.example.test_mvvm_git.databinding.ProjectListItemBinding
 import com.example.test_mvvm_git.service.model.Project
 import com.example.test_mvvm_git.view.callback.ProjectClickCallback
+import java.util.*
 import javax.annotation.Nullable
 
 class ProjectAdapter (private val projectClickCallback: ProjectClickCallback):
     RecyclerView.Adapter<ProjectAdapter.Companion.ProjectViewHolder>() {
 
-    internal var projectList: List<Project>? = null
+    var listProject: List<Project>? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectViewHolder {
         val binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.project_list_item, parent, false) as ProjectListItemBinding
 
-        binding.setCallback(projectClickCallback)
+        binding.callback = projectClickCallback
 
         return ProjectViewHolder(binding)
     }
 
+    fun setProjectList(projectList: List<Project>?) {
+        if (this.listProject == null) {
+            this.listProject = projectList
+            notifyItemRangeInserted(0, projectList!!.size)
+        } else {
+            val result = DiffUtil.calculateDiff(object: DiffUtil.Callback() {
+
+                override fun getOldListSize(): Int {
+                    return listProject!!.size
+                }
+
+                override fun getNewListSize(): Int {
+                    return projectList!!.size
+                }
+
+                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                    return listProject!![oldItemPosition].id == projectList!![newItemPosition].id
+                }
+
+                override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                    val project = projectList!![newItemPosition]
+                    val old = projectList[oldItemPosition]
+
+                    return project.id == old.id && Objects.equals(project.git_url, old.git_url)
+                }
+            })
+            this.listProject = projectList
+            result.dispatchUpdatesTo(this)
+        }
+    }
+
     override fun onBindViewHolder(holder: ProjectViewHolder, position: Int) {
-        holder.binding.setProject(projectList!![position])
+        holder.binding.project = listProject!![position]
         holder.binding.executePendingBindings()
     }
 
     override fun getItemCount(): Int {
-        return if (projectList == null) 0 else projectList!!.size
+        return if (listProject == null) 0 else listProject!!.size
     }
 
     companion object {
