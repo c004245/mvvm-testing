45075a3fe586d4d611e0f19b564e5a8ee9533480
hyunwook <c004245@naver.com>
2019-09-06 17:11:10 +0900

    Set List Project xml

--- test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/view/ProjectListFragment.kt
+++ test-mvvm-git/app/src/main/java/com/example/test_mvvm_git/view/ProjectListFragment.kt
@@ -1,19 +1,29 @@
 package com.example.test_mvvm_git.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.test_mvvm_git.R
+import com.example.test_mvvm_git.di.Injectable
 import com.example.test_mvvm_git.service.model.Project
 import com.example.test_mvvm_git.viewmodel.ProjectListViewModel
 
-class ProjectListFragment: Fragment {
+class ProjectListFragment: Fragment(), Injectable {
+
     lateinit var projectAdapter: ProjectAdapter
+    lateinit var binding: FragmentProjectListBinding
 
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_project)
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
         val viewModel = ViewModelProviders.of(this).get(ProjectListViewModel::class.java)
 
         observeViewModel(viewModel)
